8f99448385b963347da0f29252dbce7c3a278ee1
David Barroso <dbarroso@spotify.com>
2015-03-05 16:33:48 +0100

    asd

--- objects/interface.py
+++ objects/interface.py
@@ -1,21 +1,21 @@
-class Status:
-    UP = 0
-    OPER_DOWN = 1
-    ADMIN_DOWN = 2
-
+class InterfaceStatus:
+    UP = 1
+    OPER_DOWN = -1
+    ADMIN_DOWN = -2
 
 class Interface:
-    def __init__(self, name, speed, interface_type, status):
+    def __init__(self, name, speed, status):
         """
         Represents an interface on a switch.
 
         :param name: (str) Name of the interface
         :param speed: (int) Speed of the interface in Gbps.
-        :param interface_type: (str) Type of the interface, i.e.
         :param status: (Status) Status of an interface. Make sure you use the values on the class Status.
         :return:
         """
-        self.name
-        self.speed
-        self.type
-        self.interface_type
+        self.name = name
+        self.speed = speed
+        self.status = status
+
+    def __str__(self):
+        return self.name
