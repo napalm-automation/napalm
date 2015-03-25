5afd292f974a361adfaa997185f8070e326adea2
David Barroso <dbarroso@spotify.com>
2015-03-25 12:42:10 +0100

    Unified unittests

--- test/unit/TestEOSDriver.py
+++ test/unit/TestEOSDriver.py
@@ -1,4 +1,4 @@
-# Copyright 2014 Spotify AB. All rights reserved.
+# Copyright 2015 Spotify AB. All rights reserved.
 #
 # The contents of this file are licensed under the Apache License, Version 2.0
 # (the "License"); you may not use this file except in compliance with the
@@ -15,61 +15,18 @@
 import unittest
 
 from napalm.eos import EOSDriver
-from objects.facts import Facts
-from objects.interface import Interface
-from objects.bgp import BGPInstance
+from base import TestNetworkDriver
 
-import config
+skip=False
 
-
-class TestEOSDriver(unittest.TestCase):
+class TestEOSDriver(unittest.TestCase, TestNetworkDriver):
 
     @classmethod
     def setUpClass(cls):
-        cls.device = EOSDriver(config.hostname, config.username, config.password)
+        hostname = '192.168.76.10'
+        username = 'dbarroso'
+        password = 'this_is_not_a_secure_password'
+
+        cls.device = EOSDriver(hostname, username, password)
         cls.device.open()
-
-        with open(config.config_file_1, 'r') as f:
-            cls.config_1 = f.readlines()
-
-    @classmethod
-    def tearDownClass(cls):
-        cls.device.close()
-
-    def test_loading_config(self):
-        self.device.load_replace_candidate(filename=config.config_file_1)
-        self.device.commit_config()
-        diff = self.device.compare_config()
-        self.assertEqual(len(diff), 0)
-
-    def test_loading_modified_config_and_diff(self):
-        self.device.load_replace_candidate(filename=config.config_file_2)
-        diff = self.device.compare_config()
-        self.assertGreater(len(diff), 0)
-
-    def test_loading_modified_config_replace_config_and_rollback(self):
-        self.device.load_replace_candidate(filename=config.config_file_2)
-        orig_diff = self.device.compare_config()
-        self.device.commit_config()
-        replace_config_diff = self.device.compare_config()
-        self.device.rollback()
-        last_diff = self.device.compare_config()
-
-        result = (orig_diff == last_diff) and (len(replace_config_diff) == 0)
-
-        self.assertTrue(result)
-
-    def test_get_facts(self):
-        facts = self.device.get_facts()
-        self.assertIsInstance(facts, Facts)
-
-    def test_get_bgp_neighbors(self):
-        bgp_table = self.device.get_bgp_neighbors()
-        self.assertIsInstance(bgp_table[0], BGPInstance)
-
-    def test_get_interface(self):
-        interface = self.device.get_interface('Ethernet1')
-        self.assertIsInstance(interface, Interface)
-
-
-
+        cls.vendor = 'eos'
