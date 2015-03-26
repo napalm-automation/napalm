e1f86167afc61b65ac31ede57b40cab5e2beb0c1
David Barroso <dbarroso@spotify.com>
2015-03-26 14:17:45 +0100

    Minor improvements

---
+++ test/unit/TestIOSXRDriver.py
@@ -0,0 +1,31 @@
+# Copyright 2015 Spotify AB. All rights reserved.
+#
+# The contents of this file are licensed under the Apache License, Version 2.0
+# (the "License"); you may not use this file except in compliance with the
+# License. You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations under
+# the License.
+
+import unittest
+
+from napalm.eos import EOSDriver
+from base import TestNetworkDriver
+
+
+class TestEOSDriver(unittest.TestCase, TestNetworkDriver):
+
+    @classmethod
+    def setUpClass(cls):
+        hostname = '192.168.76.10'
+        username = 'dbarroso'
+        password = 'this_is_not_a_secure_password'
+        cls.vendor = 'eos'
+
+        cls.device = EOSDriver(hostname, username, password)
+        cls.device.open()
