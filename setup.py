4b63334d4fb01c1ef4ec8aa04675c6b4e09cc157
David Barroso <dbarroso@spotify.com>
2015-03-27 14:47:06 +0100

    Making things ready for first release

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ reqs = [str(ir.req) for ir in install_reqs]
 
 setup(
     name="napalm",
-    version="0.10",
+    version="0.1",
     packages=find_packages(),
     author="David Barroso",
     author_email="dbarroso@spotify.net",
