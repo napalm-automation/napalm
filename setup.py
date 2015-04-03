5f53c92690be6e4821aa8dba24c410662b086ba8
David Barroso <dbarroso@spotify.com>
2015-04-03 15:24:45 +0200

    Bugfixes

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ reqs = [str(ir.req) for ir in install_reqs]
 
 setup(
     name="napalm",
-    version="0.10",
+    version="0.11",
     packages=find_packages(),
     author="David Barroso",
     author_email="dbarroso@spotify.net",
