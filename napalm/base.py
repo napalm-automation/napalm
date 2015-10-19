c514ca5c99476828d7d1347bdd5ac2b6f714af95
David Barroso <dbarrosop@dravetech.com>
2015-10-19 15:08:56 +0200

    Added timeout to base for description

--- napalm/base.py
+++ napalm/base.py
@@ -15,7 +15,7 @@
 
 class NetworkDriver:
 
-    def __init__(self, hostname, username, password):
+    def __init__(self, hostname, username, password, timeout):
         """
         This is the base class you have to inherit from when writing your own Network Driver to manage any device. You
         will, in addition, have to override all the methods specified on this class. Make sure you follow the guidelines
@@ -24,6 +24,7 @@ class NetworkDriver:
         :param hostname: (str) IP or FQDN of the device you want to connect to.
         :param username: (str) Username you want to use
         :param password: (str) Password
+        :param timeout: (int) Time in seconds to wait for the device to respond.
         :return:
         """
         raise NotImplementedError
