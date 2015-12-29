e403502248efb9e704ca96399dc686f1a1f2ec70
Rickard Östman <rost@spotify.com>
2015-12-29 15:29:57 +0100

    allowing main environment-query plenty of time to finish

--- napalm/iosxr.py
+++ napalm/iosxr.py
@@ -387,7 +387,7 @@ class IOSXRDriver(NetworkDriver):
         
         ## finding slots with equipment we're interested in
         main_query = "<Get><AdminOperational><EnvironmentalMonitoring></EnvironmentalMonitoring></AdminOperational></Get>"
-        main_result_tree = ET.fromstring(self.device.make_rpc_call(main_query))
+        main_result_tree = ET.fromstring(self.device.make_rpc_call(main_query,180))
 
         active_modules = defaultdict(list)
 
