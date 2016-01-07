e9af2d3cadacd1e0199fc372436ea0bbebaf14be
Kirk Byers <ktbyers@twb-tech.com>
2016-01-06 21:35:58 -0800

    Add support for optional arguments

--- napalm/iosxr.py
+++ napalm/iosxr.py
@@ -25,7 +25,9 @@ import re
 
 
 class IOSXRDriver(NetworkDriver):
-    def __init__(self, hostname, username, password, timeout=60):
+    def __init__(self, hostname, username, password, timeout=60, optional_args=None):
+        if optional_args is None:
+            optional_args = {}
         self.hostname = hostname
         self.username = username
         self.password = password
