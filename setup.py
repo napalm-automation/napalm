73d288aa1b60c47dbc6c8e4c52edd49073c73521
David Barroso <dbarrosop@dravetech.com>
2016-03-04 13:09:46 +0100

    Bump version to 0.60.0

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ reqs = [str(ir.req) for ir in install_reqs]
 
 setup(
     name="napalm",
-    version="0.51.0",
+    version="0.60.0",
     packages=find_packages(),
     author="David Barroso",
     author_email="dbarrosop@dravetech.com",
