a6669718e5787bec4f55a7d3e84eb81988fcc624
David Barroso <dbarrosop@dravetech.com>
2016-03-21 10:47:24 +0100

    Added driver to the basic module so it can be discovered

--- napalm_nxos/__init__.py
+++ napalm_nxos/__init__.py
@@ -12,4 +12,5 @@
 # License for the specific language governing permissions and limitations under
 # the License.
 
-"""napalm_ios package."""
+"""napalm_nxos package."""
+from nxos import NXOSDriver
