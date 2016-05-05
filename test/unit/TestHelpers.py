73976ee372a3994b7791d03a30d58c8d9e1e4e45
Mircea Ulinic <mirucha@cloudflare.com>
2016-05-05 18:13:51 +0000

    Test also inexisting template from custom path

--- test/unit/TestHelpers.py
+++ test/unit/TestHelpers.py
@@ -50,6 +50,8 @@ class TestBaseHelpers(unittest.TestCase):
             * check if raises TemplateRenderException when template is not correctly formatted
             * check if can load correct template
             * check if can load correct template even if wrong custom path specified
+            * check if raises TemplateNotImplemented when trying to use inexisting template in custom path
+            * check if can load correct template from custom path
         """
 
         self.assertTrue(HAS_JINJA)  # firstly check if jinja2 is installed
@@ -88,7 +90,14 @@ class TestBaseHelpers(unittest.TestCase):
                                                           **_TEMPLATE_VARS))
 
         install_dir = os.path.dirname(os.path.abspath(sys.modules[self.network_driver.__module__].__file__))
-        custom_path = os.path.join(install_dir, 'custom/path/base')
+        custom_path = os.path.join(install_dir, 'test/custom/path/base')
+
+        self.assertRaises(napalm_base.exceptions.TemplateNotImplemented,
+                          napalm_base.helpers.load_template,
+                          self.network_driver,
+                          '__this_template_does_not_exist__',
+                          template_path=custom_path,
+                          **_TEMPLATE_VARS)
 
         self.assertTrue(napalm_base.helpers.load_template(self.network_driver,
                                                           '__a_very_nice_template__',
