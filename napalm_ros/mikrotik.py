aef2d6aea562f585fc749924085f7402414b6f68
Matt Ryan <inetuid@gmail.com>
2016-06-14 14:28:04 +0100

    Updated version.

--- napalm_ros/mikrotik.py
+++ napalm_ros/mikrotik.py
@@ -1,5 +1,7 @@
 from napalm_base.base import NetworkDriver
-import yandca.mikrotik
+from napalm_base.exceptions import ConnectionException, SessionLockedException, MergeConfigException, ReplaceConfigException, CommandErrorException
+import napalm_base.utils
+import yandc.mikrotik
 import json
 import re
 
@@ -11,14 +13,15 @@ class ROSDriver(NetworkDriver):
 		self.password = password
 		self.timeout = timeout
 
-		if 'snmp_community' in optional_args:
-			self.snmp_community = optional_args['snmp_community']
-		if 'snmp_port' in optional_args:
-			self.snmp_port = optional_args['snmp_port']
+		if isinstance(optional_args, dict):
+			self.snmp_community = optional_args.get('snmp_community', None)
+			self.snmp_port = optional_args.get('snmp_port', 161)
 		
 	def open(self):
-		self.device = yandca.mikrotik.ROS_Client(
+		self.device = yandc.mikrotik.ROS_Client(
 			host=self.hostname,
+			snmp_community=self.snmp_community,
+			snmp_port=self.snmp_port,
 			ssh_username=self.username,
 			ssh_password=self.password
 		)
@@ -36,34 +39,69 @@ class ROSDriver(NetworkDriver):
 		del self.device
 
 	def get_arp_table(self):
-		arp_show_command = ':put [/ip arp print as-value]'
-		cli_output = self.cli([arp_show_command])
-
 		arp_table = list()
-		for arp_entry in self.device.values_decode(cli_output.get(arp_show_command)[0]):
+		for arp_entry in self.device.print_to_values_structured(self.device.ssh_command('/ip arp print terse without-paging')):
 			arp_table.append(
 				{
-					unicode('interface'): arp_entry.get('interface'),
-					unicode('mac'): arp_entry.get('mac-address'),
-					unicode('ip'): arp_entry.get('address'),
+					unicode('interface'): arp_entry['interface'],
+					unicode('mac'): arp_entry['mac-address'],
+					unicode('ip'): arp_entry['address'],
 					unicode('age'): -1,
 				}
 			)
-
 		return arp_table
 
 	def get_bgp_neighbors(self):
+		bgp_neighbors = {}
+		if not self.device.system_package_enabled('routing'):
+			return bgp_neighbors
+
+
+		peer_print_status = self.device.ssh_command('/routing bgp peer print status without-paging')
+		peer_print_status.pop(0)
+		peer_status_values = self.device.print_to_values_structured(self.device.print_concat(peer_print_status))
+		peer_status_values_indexed = self.device.index_values(peer_status_values, 'instance')
+
+		for bgp_instance in self.device.print_to_values_structured(self.device.ssh_command('/routing bgp instance print terse without-paging')):
+			instance_name = bgp_instance['name']
+			if instance_name in bgp_neighbors:
+				raise Exception('Key already seen')
+			bgp_neighbors[instance_name] = {
+				'router_id': bgp_instance['router-id']
+			}
+			for bgp_peer in peer_status_values_indexed[instance_name]:
+				bgp_neighbors[instance_name][bgp_peer['remote-address']] = {
+					'local_as': bgp_instance['as'],
+					'remote_as': bgp_peer['remote-as'],
+					'remote_id': bgp_peer['remote-id'],
+					'is_up': bgp_peer['state'] == 'established',
+					'is_enabled': bgp_peer['flags'].find('X') == -1,
+					'description': bgp_peer['name'].replace('"', ''),
+					'uptime': self.device.to_seconds(bgp_peer['uptime']),
+					'address_family': {
+						'ipv4': {
+							'received_prefixes': bgp_peer['prefix-count'],
+							'accepted_prefixes': 0,
+							'sent_prefixes': 0
+						},
+						'ipv6': {
+							'received_prefixes': 0,
+							'accepted_prefixes': 0,
+							'sent_prefixes': 0
+						}
+					}
+				}
+		return bgp_neighbors
+
+	def get_bgp_neighbors_detail(self, neighbor_address=''):
 		pass
 
 	def get_environment(self):
-		system_resource = self.device.system_resource()
-		system_health = self.device.system_health()
+		system_resource_values = self.device.print_to_values(self.device.ssh_command('/system resource print without-paging'))
+		system_health_values = self.device.print_to_values(self.device.ssh_command('/system health print without-paging'))
 
-		cpu_load = {}
-		for i in self.device.system_resource_cpu():
-			cpu_load[i.get('cpu')] = {
-				'%usage': i.get('load', -1)
-			}
+		cpu_load = dict((v['cpu'], v['load'].rstrip('%')) for v in self.device.print_to_values_structured(self.device.ssh_command('/system resource cpu print terse without-paging')))
+		print cpu_load
 
 		return {
 			'fans': {
@@ -73,7 +111,7 @@ class ROSDriver(NetworkDriver):
 			},
 			'temperature': {
 				'board': {
-					'temperature': system_health.get('temperature', -1.0),
+					'temperature': float(system_health_values.get('temperature', '-1.0').rstrip('C')),
 					'is_alert': False,
 					'is_critical': False,
 				}
@@ -82,58 +120,169 @@ class ROSDriver(NetworkDriver):
 				'main': {
 					'status': True,
 					'capacity': 0.0,
-					'output': system_health.get('voltage', 0.0)
+					'output': float(system_health_values.get('voltage', '0.0').rstrip('V'))
 				}
 			},
 			'cpu': cpu_load,
 			'memory': {
-				'available_ram': int(system_resource.get('total-memory')),
-				'used_ram': int(system_resource.get('total-memory')) - int(system_resource.get('free-memory'))
+				'available_ram': float(re.sub('[KM]iB$', '', system_resource_values.get('total-memory'))),
+#				'used_ram': int(system_resource_values.get('total-memory')) - int(system_resource_values.get('free-memory'))
 			}
 		}
 
 	def get_facts(self):
-		system_routerboard_command = ':put [/system routerboard print as-value]'
-		system_identity_command = ':put [/system identity print as-value]'
-		cli_output = self.cli([system_routerboard_command, system_identity_command])
-		system_resource = self.device.system_resource()
+		system_resource = self.device.print_to_values(self.device.ssh_command('/system resource print without-paging'))
+		system_identity = self.device.print_to_values(self.device.ssh_command('/system identity print without-paging'))
+		system_routerboard = self.device.print_to_values(self.device.ssh_command('/system routerboard print without-paging'))
 
 		return {
-			'uptime': system_resource.get('uptime'),
-			'vendor': system_resource.get('platform'),
-			'model': system_resource.get('board-name'),
-			'hostname': '',
+			'uptime': self.device.to_seconds(system_resource['uptime']),
+			'vendor': system_resource['platform'],
+			'model': system_resource['board-name'],
+			'hostname': system_identity['name'],
 			'fqdn': '',
-			'os_version': system_resource.get('version'),
-			'serial_number': None,
-			'interface_list': []
+			'os_version': system_resource['version'],
+			'serial_number': system_routerboard['serial-number'] if system_routerboard['routerboard'] == 'yes' else '',
+			'interface_list': napalm_base.utils.string_parsers.sorted_nicely(self.device.interfaces())
 		}
 
 	def get_interfaces(self):
-		pass
+		interfaces = {}
+		for if_entry in self.device.print_to_values_structured(self.device.ssh_command('/interface print terse without-paging')):
+			interfaces[unicode(if_entry['name'])] = {
+				'is_up': if_entry['flags'].find('R') != -1,
+				'is_enabled': if_entry['flags'].find('X') == -1,
+				'description': if_entry.get('comment', ''),
+				'last_flapped': self.device.to_seconds_date_time(if_entry.get('last-link-up-time', '')),
+				'speed': -1,
+				'mac_address': if_entry.get('mac-address', '')
+			}
+		return interfaces
 
 	def get_interfaces_counters(self):
-		pass
+		stats_detail = self.device.ssh_command('interface print stats-detail without-paging')
+		stats_detail.pop(0)
+
+		interface_counters = {}
+		for if_counters in self.device.print_to_values_structured(self.device.print_concat(stats_detail)):
+			if_name = unicode(if_counters['name'].replace('"', ''))
+			if if_name in if_counters:
+				raise Exception('Interface already seen')
+			interface_counters[if_name] = {
+				'tx_errors': int(if_counters['tx-error'].replace(' ', '')),
+				'rx_errors': int(if_counters['rx-error'].replace(' ', '')),
+				'tx_discards': int(if_counters['tx-drop'].replace(' ', '')),
+				'rx_discards': int(if_counters['rx-drop'].replace(' ', '')),
+				'tx_octets': int(if_counters['tx-byte'].replace(' ', '')),
+				'rx_octets': int(if_counters['rx-byte'].replace(' ', '')),
+				'tx_unicast_packets': -1,
+				'rx_unicast_packets': -1,
+				'tx_multicast_packets': -1,
+				'rx_multicast_packets': -1,
+				'tx_broadcast_packets': -1,
+				'rx_broadcast_packets': -1
+			}
+		return interface_counters
+
+	def get_interfaces_ip(self):
+		ipv4_address_values = self.device.print_to_values_structured(self.device.ssh_command('/ip address print terse without-paging'))
+		ipv6_address_values = self.device.print_to_values_structured(self.device.ssh_command('/ipv6  address print terse without-paging'))
+
+		interfaces_ip = {}
+		for key, value in self.device.index_values(ipv4_address_values, 'interface').iteritems():
+			if_name = unicode(key)
+			if not if_name in interfaces_ip:
+				interfaces_ip[if_name] = {
+					u'ipv4': {}
+				}
+			for v in value:
+				ipv4_address, prefix_length = v['address'].split('/', 1)
+				interfaces_ip[if_name][u'ipv4'][unicode(ipv4_address)] = dict(prefix_length=int(prefix_length))
+
+		for key, value in self.device.index_values(ipv6_address_values, 'interface').iteritems():
+			if_name = unicode(key)
+			if not if_name in interfaces_ip:
+				interfaces_ip[if_name] = {
+					u'ipv6': {}
+				}
+			for v in value:
+				ipv6_address, prefix_length = v['address'].split('/', 1)
+				if not u'ipv6' in interfaces_ip[if_name]:
+					interfaces_ip[if_name][u'ipv6'] = {}
+				interfaces_ip[if_name][u'ipv6'][unicode(ipv6_address)] = dict(prefix_length=int(prefix_length))
+
+		return interfaces_ip
 
 	def get_lldp_neighbors(self):
-		pass
+		terse_values = self.device.print_to_values_structured(self.device.ssh_command('/ip neighbor print terse without-paging'))
 
-	def get_lldp_neighbors_detail(self):
-		pass
+		lldp_neighbors = {}
+		for key, value in self.device.index_values(terse_values, 'interface').iteritems():
+			if_name = unicode(key)
+			if if_name in lldp_neighbors:
+				raise Exception('Key already seen')
+			lldp_neighbors[if_name] = []
+			for v in value:
+				lldp_neighbors[if_name].append(
+					{
+						'hostname': unicode(v['identity']),
+						'port': unicode(v['interface-name'])
+					}
+				)
+		return lldp_neighbors
+
+	def get_lldp_neighbors_detail(self, interface=''):
+		ip_neighbor_command = '/ip neighbor print terse without-paging'
+		if interface != '':
+			ip_neighbor_command += ' where interface="{}"'.format(interface)
+		terse_values = self.device.print_to_values_structured(self.device.ssh_command(ip_neighbor_command))
+
+		lldp_neighbors_detail = {}
+		for key, value in self.device.index_values(terse_values, 'interface').iteritems():
+			if_name = unicode(key)
+			if if_name in lldp_neighbors_detail:
+				raise Exception('Key already seen')
+			lldp_neighbors_detail[if_name] = []
+			for v in value:
+				lldp_neighbors_detail[if_name].append(
+					{
+						'parent_interface': None,
+						'remote_chassis_id': v['mac-address'],
+						'remote_system_name': v['identity'],
+						'remote_port': v['interface-name'],
+						'remote_port_description': '',
+						'remote_system_description': '{} {}'.format(v['platform'], v['board']),
+						'remote_system_capab': None,
+						'remote_system_enable_capab': None
+					}
+				)
+		return lldp_neighbors_detail
+
+	def get_mac_address_table(self):
+		mac_address_table = []
+		for mac_entry in self.device.print_to_values_structured(self.device.ssh_command('/interface ethernet switch host print terse without-paging')):
+			mac_address_table.append(
+				{
+					'mac': mac_entry['mac-address'],
+					'interface': mac_entry['ports'],
+					'vlan': mac_entry.get('vlan-id', 0),
+					'static': mac_entry['flags'].find('D') == -1,
+					'active': True,
+					'moves': -1,
+					'last_move': 0.0
+				}
+			)
+		return mac_address_table
 
 	def get_snmp_information(self):
-		snmp_command = ':put [/snmp print as-value]'
-		snmp_community_command = ':put [/snmp community print as-value]'
-		cli_output = self.cli([snmp_command, snmp_community_command])
-		snmp_values = self.device.values_decode(cli_output.get(snmp_command)[0])[0]
-		snmp_community_values = self.device.values_decode(cli_output.get(snmp_community_command)[0])
-		print snmp_community_values
+		snmp_values = self.device.print_to_values(self.device.ssh_command('/snmp print without-paging'))
+		snmp_community_values = self.device.print_to_values_structured(self.device.ssh_command('/snmp community print terse without-paging'))
 
 		snmp_communities = {}
-		for comm in snmp_community_values:
-			snmp_communities[unicode(comm.get('name'))] = {
-				'acl': unicode(comm.get('addresses', '')),
-				'mode': 'ro' if comm.get('read-access', '') == 'true' else 'rw'
+		for v in snmp_community_values:
+			snmp_communities[unicode(v.get('name'))] = {
+				'acl': unicode(v.get('addresses', '')),
+				'mode': 'ro' if v.get('read-access', '') == 'yes' else 'rw'
 			}
 
 		return {
@@ -144,19 +293,33 @@ class ROSDriver(NetworkDriver):
 		}
 
 	def get_users(self):
-		user_command = ':put [/user print as-value]'
-		user_sshkeys_command = ':put [/user ssh-keys print as-value]'
-		cli_output = self.cli([user_command, user_sshkeys_command])
-		user_values = self.device.values_decode(cli_output.get(user_command)[0])
-		user_sshkeys_values = self.device.values_decode(cli_output.get(user_sshkeys_command)[0])
-		print user_sshkeys_values
+		user_sshkeys_values = self.device.print_to_values_structured(self.device.ssh_command('/user ssh-keys print terse without-paging'))
+		user_sshkeys_values_indexed = self.device.index_values(user_sshkeys_values, 'user')
 
 		users = {}
-		for user in user_values:
-			users[user.get('name')] = {
-				'level': 15 if user.get('group') == 'full' else 0,
-				'password': None,
-				'sshkeys': []
+		for u in self.device.print_to_values_structured(self.device.ssh_command('/user print terse without-paging')):
+			users[u['name']] = {
+				'level': 15 if u['group'] == 'full' else 0,
+				'password': '',
+				'sshkeys': [s for s in user_sshkeys_values_indexed.get(u['name'], [])]
 
 			}
 		return users
+
+	def load_merge_candidate(self, filename=None, config=None):
+		pass
+
+	def load_replace_candidate(self, filename=None, config=None):
+		pass
+
+	def load_template(self, template_name, template_source=None, template_path=None, **template_vars):
+		pass
+
+	def ping(destination, source='', ttl=0, timeout=0, size=0, count=0):
+		pass
+
+	def rollback(self):
+		pass
+
+	def traceroute(self, destination, source='', ttl=0, timeout=0):
+		pass
