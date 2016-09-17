8c3d4a0c11d021a96475fd368c5b892e6759f738
Mircea Ulinic <mirucha@cloudflare.com>
2016-09-17 22:38:49 +0000

    Add __version___ #41

--- napalm_junos/__init__.py
+++ napalm_junos/__init__.py
@@ -12,5 +12,15 @@
 # License for the specific language governing permissions and limitations under
 # the License.
 
-"""napalm_iosxr package."""
+"""napalm_junos package."""
+
+# Import stdlib
+import pkg_resources
+
+# Import local modules
 from junos import JunOSDriver
+
+try:
+    __version__ = pkg_resources.get_distribution('napalm-junos').version
+except pkg_resources.DistributionNotFound:
+    __version__ = "Not installed"
