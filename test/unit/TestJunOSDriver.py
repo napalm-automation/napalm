6e9be678693a34fe1bb49481d033bc9b8b24a873
Mircea Ulinic <mirucha@cloudflare.com>
2016-09-18 14:41:42 +0000

    Cleanup all and enable pylama

--- test/unit/TestJunOSDriver.py
+++ test/unit/TestJunOSDriver.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 # Copyright 2015 Spotify AB. All rights reserved.
 #
 # The contents of this file are licensed under the Apache License, Version 2.0
@@ -29,8 +30,12 @@ class TestConfigJunOSDriver(unittest.TestCase, TestConfigNetworkDriver):
         password = 'vagrant123'
         cls.vendor = 'junos'
 
-        optional_args = {'port': 12203,}
-        cls.device = JunOSDriver(hostname, username, password, timeout=60, optional_args=optional_args)
+        optional_args = {'port': 12203, }
+        cls.device = JunOSDriver(hostname,
+                                 username,
+                                 password,
+                                 timeout=60,
+                                 optional_args=optional_args)
         cls.device.open()
 
 
@@ -82,16 +87,14 @@ class FakeJunOSDevice:
             'personality': 'SRX_BRANCH'
         }
 
-
     def read_txt_file(self, filename):
         with open(filename) as data_file:
             return data_file.read()
 
-
     def cli(self, command=''):
         return self.read_txt_file(
             'junos/mock_data/{parsed_command}.txt'.format(
-                parsed_command = command.replace(' ', '_')
+                parsed_command=command.replace(' ', '_')
             )
         )
 
@@ -105,18 +108,16 @@ class FakeRPCObject:
     def __init__(self, device):
         self._device = device
 
-
     def __getattr__(self, item):
         self.item = item
         return self
 
-
     def response(self, **rpc_args):
         instance = rpc_args.pop('instance', '')
 
-        xml_string = self._device.read_txt_file('junos/mock_data/{}{}.txt'.format(self.item, instance))
+        xml_string = self._device.read_txt_file(
+            'junos/mock_data/{}{}.txt'.format(self.item, instance))
         return lxml.etree.fromstring(xml_string)
-
 
     def get_config(self, get_cmd='', options={}):
 
@@ -124,15 +125,18 @@ class FakeRPCObject:
         # E.g.: <configuration><protocols><bgp><group/></bgp></protocols></configuration>
 
         get_cmd_str = lxml.etree.tostring(get_cmd)
-        filename = get_cmd_str.replace('<', '_').replace('>', '_').replace('/', '_').replace('\n', '').replace(' ', '')
+        filename = get_cmd_str.replace('<', '_')\
+                              .replace('>', '_')\
+                              .replace('/', '_')\
+                              .replace('\n', '')\
+                              .replace(' ', '')
 
         xml_string = self._device.read_txt_file(
             'junos/mock_data/{filename}.txt'.format(
-                filename = filename[0:150]
+                filename=filename[0:150]
             )
         )
         return lxml.etree.fromstring(xml_string)
-
 
     __call__ = response
 
@@ -146,14 +150,12 @@ class FakeConnectionRPCObject:
     def __init__(self, rpc):
         self._rpc = rpc
 
-
     def response(self, non_std_command=None):
         class RPCReply:
             def __init__(self, reply):
                 self._NCElement__doc = reply
         rpc_reply = RPCReply(self._rpc.get_config(get_cmd=non_std_command))
         return rpc_reply
-
 
     __call__ = response
 
