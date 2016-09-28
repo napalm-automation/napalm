8a09372feab4269d86ca57876089ade98f5d433e
Kirk Byers <ktbyers@twb-tech.com>
2016-09-27 22:03:39 -0700

    Adding conditional import in .travis.yml for python3

--- test/unit/TestGetNetworkDriver.py
+++ test/unit/TestGetNetworkDriver.py
@@ -21,7 +21,7 @@ class TestGetNetworkDriver(unittest.TestCase):
                            'nxos', 'pluribus', 'panos')
     elif sys.version[0] == '3':
         # Drivers that support python2 and python3
-        network_drivers = ('ios',)
+        network_drivers = ()
 
     @data(*network_drivers)
     def test_get_network_driver(self, driver):
