cda90beb4d92fa26cd5d0b8140dce1963ab58cd8
Kirk Byers <ktbyers@twb-tech.com>
2016-10-14 13:59:09 -0700

    pylama cleanup add travis.yml pin napalm-base version update setup.cfg

--- napalm_ros/__init__.py
+++ napalm_ros/__init__.py
@@ -1,1 +1,14 @@
-from .ros import ROSDriver
+"""napalm_ros package."""
+
+# Import stdlib
+import pkg_resources
+
+# Import local modules
+from napalm_ros.ros import ROSDriver
+
+try:
+    __version__ = pkg_resources.get_distribution('napalm-ros').version
+except pkg_resources.DistributionNotFound:
+    __version__ = "Not installed"
+
+__all__ = ('ROSDriver',)
