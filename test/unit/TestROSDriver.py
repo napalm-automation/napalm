4ae1644a81f98d00fc36a6e86578d852e4d04be7
David Barroso <dbarrosop@dravetech.com>
2016-10-16 19:22:07 +0200

    Migrate to new testing framework

--- test/unit/TestROSDriver.py
+++ test/unit/TestROSDriver.py
@@ -6,82 +6,14 @@ from napalm_ros import ros
 from napalm_base.test.base import TestConfigNetworkDriver, TestGettersNetworkDriver  # noqa
 
 
-def command_to_filename(command):
-    command = command.lstrip('/').replace(' ', '_').replace('/', '-').replace('"', '')
-    return 'ros/mock_data/{}.out'.format(command)
-
-
-def get_mock_data(command):
-    filename = command_to_filename(command)
-    with open(filename) as f:
-        return f.read()
-
-
-# class TestConfigROSDriver(unittest.TestCase, TestConfigNetworkDriver):
-#    @classmethod
-#    def setUpClass(cls):
-#        """Executed when the class is instantiated."""
-#        ip_addr = '192.168.144.222'
-#        username = 'admin'
-#        password = 'banana12'
-#        cls.vendor = 'ros'
-#
-#        cls.device = ros.ROSDriver(ip_addr, username, password)
-#        cls.device.open()
-
-
-class TestGetterROSDriver(unittest.TestCase, TestGettersNetworkDriver):
+class TestConfigROSDriver(unittest.TestCase, TestConfigNetworkDriver):
     @classmethod
     def setUpClass(cls):
-        cls.mock = True
-
-        hostname = '127.0.0.1'
-        username = 'vagrant'
-        password = 'vagrant'
+        """Executed when the class is instantiated."""
+        ip_addr = '192.168.144.222'
+        username = 'admin'
+        password = 'banana12'
         cls.vendor = 'ros'
 
-        optional_args = {'snmp_community': 'public'}
-        cls.device = ros.ROSDriver(hostname, username, password,
-                                   timeout=60, optional_args=optional_args)
-
-        if cls.mock:
-            cls.device.paramiko_transport = FakeParamikoTransport()
-            cls.device.mikoshell = FakeMikoShell()
-            cls.device._datetime_offset = datetime.datetime.now() - datetime.datetime.now()
-        else:
-            cls.device.open()
-
-
-class FakeParamikoTransport(object):
-    def open_session(self):
-        return FakeParamikoChannel()
-
-
-class FakeMikoShell(object):
-    @staticmethod
-    def command(command, *args, **kwargs):
-        return get_mock_data(command).splitlines()
-
-
-class FakeParamikoChannel(object):
-    @staticmethod
-    def close():
-        pass
-
-    def exec_command(self, command):
-        self._exec_command = command
-
-    def makefile(self, *args):
-        return StringIO.StringIO(get_mock_data(self._exec_command))
-
-    @staticmethod
-    def set_combine_stderr(*args):
-        pass
-
-    @staticmethod
-    def shutdown(*args):
-        pass
-
-
-if __name__ == '__main__':
-    unittest.main()
+        cls.device = ros.ROSDriver(ip_addr, username, password)
+        cls.device.open()
---
+++ test/unit/conftest.py
@@ -0,0 +1,84 @@
+"""Test fixtures."""
+from builtins import super
+
+import StringIO
+import datetime
+
+import pytest
+from napalm_base.test import conftest as parent_conftest
+
+from napalm_base.test.double import BaseTestDouble
+
+from napalm_ros import ros
+
+
+@pytest.fixture(scope='class')
+def set_device_parameters(request):
+    """Set up the class."""
+    def fin():
+        request.cls.device.close()
+    request.addfinalizer(fin)
+
+    request.cls.driver = ros.ROSDriver
+    request.cls.patched_driver = PatchedROSDevice
+    request.cls.vendor = 'ros'
+    parent_conftest.set_device_parameters(request)
+
+
+def pytest_generate_tests(metafunc):
+    """Generate test cases dynamically."""
+    parent_conftest.pytest_generate_tests(metafunc, __file__)
+
+
+class PatchedROSDevice(ros.ROSDriver):
+    """ROS device test double."""
+    def __init__(self, hostname, username, password, timeout=60, optional_args=None):
+        super().__init__(hostname, username, password, timeout, optional_args)
+
+        self.patched_attrs = ['paramiko_transport', 'mikoshell']
+
+    def open(self):
+        self.paramiko_transport = FakeParamikoTransport()
+        self.mikoshell = FakeMikoShell()
+        self._datetime_offset = datetime.datetime.now() - datetime.datetime.now()
+
+
+class FakeParamikoTransport(BaseTestDouble):
+    def open_session(self):
+        return FakeParamikoChannel()
+
+    def close(self):
+        pass
+
+    def is_active(self):
+        return True
+
+
+class FakeMikoShell(BaseTestDouble):
+
+    def command(self, command, *args, **kwargs):
+        full_path = self.find_file(self.sanitize_text(command))
+        return self.read_txt_file(full_path).splitlines()
+
+    def exit(self, cmd):
+        pass
+
+
+class FakeParamikoChannel(BaseTestDouble):
+
+    def close(self):
+        pass
+
+    def exec_command(self, command):
+        self._exec_command = command
+
+    def makefile(self, *args):
+        return StringIO.StringIO(self.read_txt_file(self._exec_command))
+
+    @staticmethod
+    def set_combine_stderr(*args):
+        pass
+
+    @staticmethod
+    def shutdown(*args):
+        pass
