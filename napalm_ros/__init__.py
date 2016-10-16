22fba98b1bfa53fe1cbcb23f0843a92c0f91dd2e
David Barroso <dbarrosop@dravetech.com>
2016-10-16 13:34:09 +0200

    Fixes #8

--- napalm_ros/__init__.py
+++ napalm_ros/__init__.py
@@ -1,1 +1,10 @@
 from .ros import ROSDriver
+
+from pkg_resources import get_distribution
+
+try:
+    __version__ = pkg_resources.get_distribution('napalm-ros').version
+except pkg_resources.DistributionNotFound:
+    __version__ = "Not installed"
+
+__all__ = ('ROSDriver',)
