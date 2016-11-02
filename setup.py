03b2fe522cea657fe674b6bb889bf2cc8d0599c1
Mircea Ulinic <mirucha@cloudflare.com>
2016-11-02 18:07:33 +0000

    Release 0.3.2 & update authors

--- setup.py
+++ setup.py
@@ -12,10 +12,10 @@ reqs = [str(ir.req) for ir in install_reqs]
 
 setup(
     name="napalm-iosxr",
-    version="0.3.1",
+    version="0.3.2",
     packages=find_packages(),
-    author="David Barroso",
-    author_email="dbarrosop@dravetech.com",
+    author="David Barroso, Mircea Ulinic",
+    author_email="dbarrosop@dravetech.com, mircea@cloudflare.com",
     description="Network Automation and Programmability Abstraction Layer with Multivendor support",
     classifiers=[
         'Topic :: Utilities',
