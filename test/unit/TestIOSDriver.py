734acd88cbb41c0d11f65e602ec76b21cbf75816
Kirk Byers <ktbyers@twb-tech.com>
2016-11-13 09:40:02 -0800

    Python3 fixes to support unicode/str typecasting

--- test/unit/TestIOSDriver.py
+++ test/unit/TestIOSDriver.py
@@ -16,6 +16,7 @@
 
 import unittest
 from napalm_ios import ios
+from napalm_base.utils import py23_compat
 from napalm_base.test.base import TestConfigNetworkDriver, TestGettersNetworkDriver
 import re
 
@@ -176,7 +177,7 @@ class FakeIOSDevice:
         """Fake execute a command in the device by just returning the content of a file."""
         cmd = re.sub(r'[\[\]\*\^\+\s\|]', '_', command)
         output = self.read_txt_file('ios/mock_data/{}.txt'.format(cmd))
-        return unicode(output)
+        return py23_compat.text_type(output)
 
     def send_command(self, command):
         """Fake execute a command in the device by just returning the content of a file."""
