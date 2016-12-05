38b5d57f51fb1b7d12dbd9c65f1cd376b63ef64d
David Barroso <dbarrosop@dravetech.com>
2016-12-05 12:25:30 +0100

    Version bump (#153)

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ reqs = [str(ir.req) for ir in install_reqs]
 
 setup(
     name="napalm-base",
-    version='0.20.0',
+    version='0.20.1',
     packages=find_packages(),
     author="David Barroso",
     author_email="dbarrosop@dravetech.com",
