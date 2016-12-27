24a2db22d9b84c8873ace2af9a32495cf301e5ed
Kirk Byers <ktbyers@twb-tech.com>
2016-12-27 10:55:54 -0800

    Python3 junos_views

--- napalm_junos/utils/junos_views.py
+++ napalm_junos/utils/junos_views.py
@@ -3,5 +3,11 @@ Load tables/views
 """
 from jnpr.junos.factory import loadyaml
 from os.path import splitext
-_YAML_ = splitext(__file__)[0] + '.yml'
+from napalm_base.utils import py23_compat
+
+# Temporary fix until PYEZ provides a better solution
+if py23_compat.PY2:
+    _YAML_ = splitext(__file__)[0] + '.yml'
+else:
+    _YAML_ = splitext(__file__)[0] + '_py3.yml'
 globals().update(loadyaml(_YAML_))
