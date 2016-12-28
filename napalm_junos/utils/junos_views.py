e4850ef534d8fcf0a3637b29330bafa589389157
Kirk Byers <ktbyers@twb-tech.com>
2016-12-28 11:24:59 -0800

    Eliminate duplication of junos_views YAML file

--- napalm_junos/utils/junos_views.py
+++ napalm_junos/utils/junos_views.py
@@ -1,13 +1,28 @@
 """
 Load tables/views
 """
-from jnpr.junos.factory import loadyaml
+import yaml
+import re
+from jnpr.junos.factory import loadyaml, FactoryLoader
 from os.path import splitext
 from napalm_base.utils import py23_compat
 
-# Temporary fix until PYEZ provides a better solution
+
+def _preprocess_yml(path):
+    """Dynamically create PY3 version of the file by re-writing 'unicode' to 'str'."""
+    with open(path) as f:
+        tmp_yaml = f.read()
+    return re.sub(r"unicode", "str", tmp_yaml)
+
+
+def _loadyaml_bypass(yaml_str):
+    """Bypass Juniper's loadyaml and directly call FactoryLoader"""
+    return FactoryLoader().load(yaml.load(yaml_str))
+
+
+_YAML_ = splitext(__file__)[0] + '.yml'
 if py23_compat.PY2:
-    _YAML_ = splitext(__file__)[0] + '.yml'
+    globals().update(loadyaml(_YAML_))
 else:
-    _YAML_ = splitext(__file__)[0] + '_py3.yml'
-globals().update(loadyaml(_YAML_))
+    py3_yaml = _preprocess_yml(_YAML_)
+    globals().update(_loadyaml_bypass(py3_yaml))
