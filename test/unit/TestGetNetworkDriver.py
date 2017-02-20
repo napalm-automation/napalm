239e1947255a5ca45200cb1741ab1fe4180beb5c
David Barroso <dbarrosop@dravetech.com>
2017-02-20 12:46:05 -0800

    Deprecate IBM driver (#349)
    * Remove IBM driver
    
    * Removing napalm-ibm
    
    * Remove IBM

--- test/unit/TestGetNetworkDriver.py
+++ test/unit/TestGetNetworkDriver.py
@@ -11,7 +11,7 @@ from napalm_base import get_network_driver
 class TestGetNetworkDriver(unittest.TestCase):
     """Test the method get_network_driver."""
 
-    @data('eos', 'fortios', 'ibm', 'ios', 'iosxr', 'junos', 'nxos', 'pluribus')
+    @data('eos', 'fortios', 'ios', 'iosxr', 'junos', 'nxos', 'pluribus')
     def test_get_network_driver(self, driver):
         """Check that we can get the desired driver."""
         self.assertTrue(get_network_driver(driver))
