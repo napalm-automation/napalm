a70b33a088e11a91f5d51db11b82af7b7da03d8b
GGabriele <gabrielegerbino@gmail.com>
2017-03-01 13:11:06 +0100

    Bumping version

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ reqs = [str(ir.req) for ir in install_reqs]
 
 setup(
     name="napalm-panos",
-    version="0.4.0",
+    version="0.4.1",
     packages=find_packages(),
     author="Gabriele Gerbino",
     author_email="gabriele@networktocode.com",
