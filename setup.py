4438b1cc309e477435dc42dd8593a35f13578fca
David Barroso <dbarrosop@dravetech.com>
2017-03-03 09:06:30 +0100

    Release 1.2.0 (#354)
    * Add explanation of how the _name keyword works (#346)
    
    * Add explanation of how the _name keyword works
    
    * Fix typo
    
    * Deprecate IBM driver (#349)
    
    * Remove IBM driver
    
    * Removing napalm-ibm
    
    * Remove IBM
    
    * Bump version (#353)

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ reqs = [str(ir.req) for ir in install_reqs]
 
 setup(
     name="napalm",
-    version="1.1.0",
+    version="1.2.0",
     packages=find_packages(),
     author="David Barroso",
     author_email="dbarrosop@dravetech.com",
