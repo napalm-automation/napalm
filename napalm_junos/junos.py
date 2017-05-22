e32c258fa5fc31eb571e036b39a18f16901b0270
Mircea Ulinic <mirucha@cloudflare.com>
2017-05-22 13:13:31 +0000

    Fix #153 and optimise the implementation
    As described in #154, for newer Junos, it is possible to get the
    instance name from the BGP neighbos, so we can enhance the
    implementation and reduce the run time by executing a single request
    fetching all BGP neighbors.

--- napalm_junos/junos.py
+++ napalm_junos/junos.py
@@ -435,9 +435,20 @@ class JunOSDriver(NetworkDriver):
         return address_family
 
     def _parse_route_stats(self, neighbor):
-        data = {}
+        data = {
+            'ipv4': {
+                'received_prefixes': -1,
+                'accepted_prefixes': -1,
+                'sent_prefixes': -1
+            },
+            'ipv6': {
+                'received_prefixes': -1,
+                'accepted_prefixes': -1,
+                'sent_prefixes': -1
+            }
+        }
         if not neighbor['is_up']:
-            pass
+            return data
         elif isinstance(neighbor['tables'], list):
             for idx, table in enumerate(neighbor['tables']):
                 family = self._get_address_family(table)
@@ -464,33 +475,90 @@ class JunOSDriver(NetworkDriver):
 
     def get_bgp_neighbors(self):
         """Return BGP neighbors details."""
-        instances = junos_views.junos_route_instance_table(self.device)
-        uptime_table = junos_views.junos_bgp_uptime_table(self.device)
-        bgp_neighbors = junos_views.junos_bgp_table(self.device)
-        keys = ['local_as', 'remote_as', 'is_up', 'is_enabled', 'description', 'remote_id']
         bgp_neighbor_data = {}
-        for instance, instance_data in instances.get().items():
-            if instance.startswith('__'):
-                # junos internal instances
-                continue
-            instance_name = "global" if instance == 'master' else instance
-            bgp_neighbor_data[instance_name] = {'peers': {}}
-            for neighbor, data in bgp_neighbors.get(instance=instance).items():
-                neighbor_data = {k: v for k, v in data}
-                peer_ip = napalm_base.helpers.ip(neighbor.split('+')[0])
+        default_neighbor_details = {
+            'local_as': 0,
+            'remote_as': 0,
+            'remote_id': '',
+            'is_up': False,
+            'is_enabled': False,
+            'description': '',
+            'uptime': 0,
+            'address_family': {}
+        }
+        keys = default_neighbor_details.keys()
+
+        uptime_table = junos_views.junos_bgp_uptime_table(self.device)
+        bgp_neighbors_table = junos_views.junos_bgp_table(self.device)
+
+        uptime_table_lookup = {}
+
+        def _get_uptime_table(instance):
+            if instance not in uptime_table_lookup:
+                uptime_table_lookup[instance] = uptime_table.get(instance=instance).items()
+            return uptime_table_lookup[instance]
+
+        def _get_bgp_neighbors_core(neighbor_data, instance=None, uptime_table_items=None):
+            '''
+            Make sure to execute a simple request whenever using
+            junos > 13. This is a helper used to avoid code redundancy
+            and reuse the function also when iterating through the list
+            BGP neighbors under a specific routing instance,
+            also when the device is capable to return the routing
+            instance name at the BGP neighbor level.
+            '''
+            for bgp_neighbor in neighbor_data:
+                peer_ip = napalm_base.helpers.ip(bgp_neighbor[0].split('+')[0])
+                neighbor_details = deepcopy(default_neighbor_details)
+                neighbor_details.update(
+                    {elem[0]: elem[1] for elem in bgp_neighbor[1] if elem[1] is not None}
+                )
+                if not instance:
+                    peer_fwd_rti = neighbor_details.pop('peer_fwd_rti')
+                    instance = peer_fwd_rti
+                else:
+                    peer_fwd_rti = neighbor_details.pop('peer_fwd_rti', '')
+                instance_name = 'global' if instance == 'master' else instance
+                if instance_name not in bgp_neighbor_data:
+                    bgp_neighbor_data[instance_name] = {}
                 if 'router_id' not in bgp_neighbor_data[instance_name]:
                     # we only need to set this once
                     bgp_neighbor_data[instance_name]['router_id'] = \
-                        py23_compat.text_type(neighbor_data['local_id'])
+                        py23_compat.text_type(neighbor_details['local_id'])
                 peer = {
                     key: self._parse_value(value)
-                    for key, value in neighbor_data.items()
+                    for key, value in neighbor_details.items()
                     if key in keys
                 }
-                peer['address_family'] = self._parse_route_stats(neighbor_data)
+                peer['address_family'] = self._parse_route_stats(neighbor_details)
+                if 'peers' not in bgp_neighbor_data[instance_name]:
+                    bgp_neighbor_data[instance_name]['peers'] = {}
                 bgp_neighbor_data[instance_name]['peers'][peer_ip] = peer
-            for neighbor, uptime in uptime_table.get(instance=instance).items():
-                bgp_neighbor_data[instance_name]['peers'][neighbor]['uptime'] = uptime[0][1]
+                if not uptime_table_items:
+                    uptime_table_items = _get_uptime_table(instance)
+                for neighbor, uptime in uptime_table_items:
+                    if neighbor not in bgp_neighbor_data[instance_name]['peers']:
+                        bgp_neighbor_data[instance_name]['peers'][neighbor] = {}
+                    bgp_neighbor_data[instance_name]['peers'][neighbor]['uptime'] = uptime[0][1]
+
+        old_junos = napalm_base.helpers.convert(
+            int, self.device.facts.get('version', '0.0').split('.')[0], '0') < 13
+
+        if old_junos:
+            instances = junos_views.junos_route_instance_table(self.device)
+            for instance, instance_data in instances.get().items():
+                if instance.startswith('__'):
+                    # junos internal instances
+                    continue
+                bgp_neighbor_data[instance_name] = {'peers': {}}
+                instance_neighbors = bgp_neighbors_table.get(instance=instance).items()
+                uptime_table_items = uptime_table.get(instance=instance).items()
+                _get_bgp_neighbors_core(instance_neighbors,
+                                        instance=instance,
+                                        uptime_table_items=uptime_table_items)
+        else:
+            instance_neighbors = bgp_neighbors_table.get().items()
+            _get_bgp_neighbors_core(instance_neighbors)
         bgp_tmp_dict = {}
         for k, v in bgp_neighbor_data.items():
             if bgp_neighbor_data[k]['peers']:
