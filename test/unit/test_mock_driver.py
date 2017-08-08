42729d5129fdaaf02b45be614d2449425c91daed
bewing <nicotine@warningg.com>
2017-08-08 02:07:12 -0500

    Convert napalm_base testing to tox (#284)
    * Convert napalm_base testing to tox
    
    * Update requirements and travis config
    
    * @ogenstad to the rescue
    
    * Remove unnecessary conftest.py
    
    * Omit unit tests from coverage report
    
    * py23 text_type

--- test/unit/test_mock_driver.py
+++ test/unit/test_mock_driver.py
@@ -10,6 +10,7 @@ from __future__ import unicode_literals
 # NAPALM base
 from napalm_base import get_network_driver
 import napalm_base.exceptions
+from napalm_base.utils import py23_compat
 
 import pytest
 
@@ -39,7 +40,7 @@ class TestMockDriver(object):
 
         with pytest.raises(napalm_base.exceptions.ConnectionClosedException) as excinfo:
             d.get_facts()
-        assert "connection closed" in excinfo.value
+        assert "connection closed" in py23_compat.text_type(excinfo.value)
 
     def test_context_manager(self):
         with driver("blah", "bleh", "blih", optional_args=optional_args) as d:
@@ -60,12 +61,12 @@ class TestMockDriver(object):
         with pytest.raises(NotImplementedError) as excinfo:
             d.get_route_to()
         expected = "You can provide mocked data in {}/get_route_to.1".format(optional_args["path"])
-        assert expected in excinfo.value
+        assert expected in py23_compat.text_type(excinfo.value)
 
         with pytest.raises(NotImplementedError) as excinfo:
             d.get_route_to()
         expected = "You can provide mocked data in {}/get_route_to.2".format(optional_args["path"])
-        assert expected in excinfo.value
+        assert expected in py23_compat.text_type(excinfo.value)
 
         d.close()
 
@@ -75,15 +76,18 @@ class TestMockDriver(object):
 
         with pytest.raises(TypeError) as excinfo:
             d.get_route_to(1, 2, 3)
-        assert "get_route_to: expected at most 3 arguments, got 4" in excinfo.value
+        assert "get_route_to: expected at most 3 arguments, got 4" in py23_compat.text_type(
+                excinfo.value)
 
         with pytest.raises(TypeError) as excinfo:
             d.get_route_to(1, 1, protocol=2)
-        assert "get_route_to: expected at most 3 arguments, got 3" in excinfo.value
+        assert "get_route_to: expected at most 3 arguments, got 3" in py23_compat.text_type(
+                excinfo.value)
 
         with pytest.raises(TypeError) as excinfo:
             d.get_route_to(proto=2)
-        assert "get_route_to got an unexpected keyword argument 'proto'" in excinfo.value
+        assert "get_route_to got an unexpected keyword argument 'proto'" in py23_compat.text_type(
+                excinfo.value)
 
         d.close()
 
@@ -93,15 +97,16 @@ class TestMockDriver(object):
 
         with pytest.raises(KeyError) as excinfo:
             d.get_bgp_neighbors()
-        assert "Something" in excinfo.value
+        assert "Something" in py23_compat.text_type(excinfo.value)
 
         with pytest.raises(napalm_base.exceptions.ConnectionClosedException) as excinfo:
             d.get_bgp_neighbors()
-        assert "Something" in excinfo.value
+        assert "Something" in py23_compat.text_type(excinfo.value)
 
         with pytest.raises(TypeError) as excinfo:
             d.get_bgp_neighbors()
-            assert "Couldn't resolve exception NoIdeaException" in excinfo.value
+            assert "Couldn't resolve exception NoIdeaException" in py23_compat.text_type(
+                    excinfo.value)
 
         d.close()
 
