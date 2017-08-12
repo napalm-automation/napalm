924a02ec03b4cbac4c31c5930964a5021fabc9f0
Kirk Byers <ktbyers@twb-tech.com>
2017-08-12 09:27:57 -0700

    Working on BGP parsing

--- test/unit_ssh/mocked_data/test_get_bgp_neighbors/multiple_vrfs_ipv6/bgp_parse.py
+++ test/unit_ssh/mocked_data/test_get_bgp_neighbors/multiple_vrfs_ipv6/bgp_parse.py
@@ -1,6 +1,17 @@
 from __future__ import print_function, unicode_literals
 import re
 
+# STD REGEX PATTERNS
+IP_ADDR_REGEX = r"\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}"
+IPV4_ADDR_REGEX = IP_ADDR_REGEX
+IPV6_ADDR_REGEX_1 = r"::"
+IPV6_ADDR_REGEX_2 = r"[0-9a-fA-F:]{1,39}::[0-9a-fA-F:]{1,39}"
+IPV6_ADDR_REGEX_3 = r"[0-9a-fA-F]{1,3}:[0-9a-fA-F]{1,3}:[0-9a-fA-F]{1,3}:[0-9a-fA-F]{1,3}:" \
+                     "[0-9a-fA-F]{1,3}:[0-9a-fA-F]{1,3}:[0-9a-fA-F]{1,3}:[0-9a-fA-F]{1,3}"
+# Should validate IPv6 address using an IP address library after matching with this regex
+IPV6_ADDR_REGEX = "(?:{}|{}|{})".format(IPV6_ADDR_REGEX_1, IPV6_ADDR_REGEX_2, IPV6_ADDR_REGEX_3)
+IPV4_OR_IPV6_REGEX = "(?:{}|{})".format(IPV4_ADDR_REGEX, IPV6_ADDR_REGEX)
+
 """
         {
         "global": {
@@ -41,11 +52,41 @@ Neighbor        V    AS MsgRcvd MsgSent   TblVer  InQ OutQ Up/Down  State/PfxRcd
 10.2.1.14       4    10  472516  472238      361    0    0     3w1d 9
 10.1.0.1        4 65535  242485  242487      361    0    0    23w2d 4
     """
+
+    bgp_summary_dict = {}
+    # Check for BGP summary information lines that have no data
+    if len(bgp_summary.strip().splitlines()) <= 1:
+        return {}
+
+    allowed_afi = ['ipv4', 'ipv6']
     vrf_regex = r"^BGP summary information for VRF\s+(?P<vrf>\S+),"
     afi_regex = r"^BGP summary information.*address family (?P<afi>\S+ Unicast)"
-    match = re.search(vrf_regex, bgp_summary)
-    if match:
-        print(match.groupdict(1))
+    local_router_id = r"^BGP router identifier\s+(?P<router_id>\S+),\s+"
+
+    for pattern in [vrf_regex, afi_regex, local_router_id]:
+        match = re.search(pattern, bgp_summary, flags=re.M)
+        if match:
+            bgp_summary_dict.update(match.groupdict(1))
+
+    # Some post regex cleanup and validation
+    afi = bgp_summary_dict['afi']
+    afi = afi.split()[0].lower()
+    if afi not in allowed_afi:
+        raise ValueError("AFI ({}) is invalid and not supported.".format(afi))
+    bgp_summary_dict['afi'] = afi
+
+    match = re.search(IPV4_ADDR_REGEX, bgp_summary_dict['router_id'])
+    if not match:
+        raise ValueError("BGP router_id ({}) is not valid".format(bgp_summary_dict['router_id']))
+    print(bgp_summary_dict)
+
+    # Extract and process the tabular data
+    tabular_divider = r"^Neighbor\s+.*PfxRcd$"
+    tabular_data = re.split(tabular_divider, bgp_summary, flags=re.M)
+    if len(tabular_data) != 2:
+        raise ValueError("Unexpected data processing BGP summary information:\n\n{}".format(bgp_summary))
+    tabular_data = tabular_data[1]
+    print(tabular_data)
 
 
 f = open("show_bgp_all_summary_vrf_all.txt", "rt")
@@ -59,7 +100,7 @@ if len(bgp_summary_sections):
 for bgp_section in bgp_summary_sections:
     bgp_section = section_separator + bgp_section
     bgp_summary_parser(bgp_section)
-    break
+#    break
 
 #print(bgp_summary_sections[-3])
 
