b22a88f4f03c3f8cba4fc811287c7442cd7ccc9e
Kirk Byers <ktbyers@twb-tech.com>
2017-08-26 17:25:48 -0700

    Add is_alive function

--- napalm_nxos_ssh/nxos_ssh.py
+++ napalm_nxos_ssh/nxos_ssh.py
@@ -441,9 +441,21 @@ class NXOSSSHDriver(NetworkDriver):
             return "{0}\n{1}".format(pattern.format(filename), string)
 
     def is_alive(self):
+        """Returns a flag with the state of the SSH connection."""
+        null = chr(0)
+        try:
+            if self.device is None:
+                return {'is_alive': False}
+            else:
+                # Try sending ASCII null byte to maintain the connection alive
+                self.device.send_command(null)
+        except (socket.error, EOFError):
+            # If unable to send, we can tell for sure that the connection is unusable,
+            # hence return False.
+            return {'is_alive': False}
         return {
             'is_alive': self.device.remote_conn.transport.is_active()
-            }
+        }
 
     def load_replace_candidate(self, filename=None, config=None):
         self._replace_candidate(filename, config)
