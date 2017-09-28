9f5588d1d9e346194b60f749ee8f86c8df430692
Carles Kishimoto <carles.kishimoto@gmail.com>
2017-09-28 17:10:22 +0200

    fix send_prefixes funciton based on send-state

--- napalm_junos/junos.py
+++ napalm_junos/junos.py
@@ -515,6 +515,21 @@ class JunOSDriver(NetworkDriver):
                 uptime_table_lookup[instance] = uptime_table.get(instance=instance).items()
             return uptime_table_lookup[instance]
 
+        def _fix_sent_prefixes(neighbor_details):
+
+            print(neighbor_details)
+            if (type(neighbor_details['tables']) == type([])):
+                # Multiple tables => we need to fix sent_prefixes based on send-state
+                sent_prefixes_list = list()
+                for state in neighbor_details['send-state']:
+                    if 'in sync' in state:
+                        sent_prefixes_list.append(neighbor_details['sent_prefixes'].pop(0))
+                    else:
+                        sent_prefixes_list.append(0)
+                neighbor_details['sent_prefixes'] = sent_prefixes_list
+            print(neighbor_details)
+            return
+
         def _get_bgp_neighbors_core(neighbor_data, instance=None, uptime_table_items=None):
             '''
             Make sure to execute a simple request whenever using
@@ -530,6 +545,9 @@ class JunOSDriver(NetworkDriver):
                 neighbor_details.update(
                     {elem[0]: elem[1] for elem in bgp_neighbor[1] if elem[1] is not None}
                 )
+                
+                neighbor_details = _fix_sent_prefixes(neighbor_details)
+
                 if not instance:
                     # not instance, means newer Junos version,
                     # as we request everything in a single request
