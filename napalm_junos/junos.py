cf120ee6a664fce5f504d9c7e11dc33c2ca844d6
Carles Kishimoto <carles.kishimoto@gmail.com>
2017-09-29 15:11:07 +0200

    adding comments formatting 40G data

--- napalm_junos/junos.py
+++ napalm_junos/junos.py
@@ -1763,8 +1763,10 @@ class JunOSDriver(NetworkDriver):
         optics_table.get()
         optics_items = optics_table.items()
 
-        # Format data inserting lane 0 for single lane optics
-        # Format: [(iface, lane, [optical_values]), ...]
+        # optics_items has no lane information, so we need to re-format data
+        # inserting lane 0 for all optics. Note it contains all optics 10G/40G/100G
+        # but the information for 40G/100G is incorrect at this point
+        # Example: intf_optic item is now: ('xe-0/0/0', [ optical_values ])
         optics_items_with_lane = []
         for intf_optic_item in optics_items:
             a = list(intf_optic_item)
@@ -1772,26 +1774,35 @@ class JunOSDriver(NetworkDriver):
             new_intf_optic_item = tuple(a)
             optics_items_with_lane.append(new_intf_optic_item)
 
+        # Now optics_items_with_lane has all optics with lane 0 included
+        # Example: ('xe-0/0/0', u'0', [ optical_values ])
+
         # Get optical information for 40G/100G optics
         optics_table40G = junos_views.junos_intf_40Goptics_table(self.device)
         optics_table40G.get()
         optics_40Gitems = optics_table40G.items()
 
-        # Format data inserting lane as before.
-        # Format: [(iface, lane, [optical_values]), ...]
+        # Re-format data as before inserting lane value
         new_optics_40Gitems = []
         for item in optics_40Gitems:
             lane = item[0]
             iface = item[1].pop(0)
             new_optics_40Gitems.append((iface[1], py23_compat.text_type(lane), item[1]))
 
-        # Remove 40G optics returned from junos_intf_optics_table with all values to 0
+        # New_optics_40Gitems contains 40G/100G optics only:
+        # ('et-0/0/49', u'0', [ optical_values ]),
+        # ('et-0/0/49', u'1', [ optical_values ]),
+        # ('et-0/0/49', u'2', [ optical_values ])
+
+        # Remove 40G/100G optics entries with wrong information returned
+        # from junos_intf_optics_table()
         iface_40G = [item[0] for item in new_optics_40Gitems]
         for intf_optic_item in optics_items_with_lane:
             iface_name = intf_optic_item[0]
             if iface_name not in iface_40G:
                 new_optics_40Gitems.append(intf_optic_item)
 
+        # New_optics_40Gitems contains all optics 10G/40G/100G with the lane
         optics_detail = {}
         for intf_optic_item in new_optics_40Gitems:
             lane = intf_optic_item[1]
