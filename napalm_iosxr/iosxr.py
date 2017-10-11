c665e9765e4552a4e827f95cf39a148db5d750a9
Mircea Ulinic <mirucha@cloudflare.com>
2017-10-11 14:13:22 +0000

    Just forward the flag from Netmiko

--- napalm_iosxr/iosxr.py
+++ napalm_iosxr/iosxr.py
@@ -107,17 +107,10 @@ class IOSXRDriver(NetworkDriver):
 
     def is_alive(self):
         """Returns a flag with the state of the connection."""
-        null = chr(0)
-        if self.device is None:
-            return {'is_alive': False}
-        try:
-            # Try sending ASCII null byte to maintain the connection alive
-            self.device.device.write_channel(null)
-            return {'is_alive': self.device.device.remote_conn.transport.is_active()}
-        except (socket.error, EOFError):
-            # If unable to send, we can tell for sure that the connection is unusable
-            return {'is_alive': False}
-        return {'is_alive': False}
+        # Simply returns the flag from Netmiko
+        return {
+            'is_alive': self.device.device.is_alive()
+        }
 
     def load_replace_candidate(self, filename=None, config=None):
         self.pending_changes = True
