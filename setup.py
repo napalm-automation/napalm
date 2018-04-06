ebd5591e365e77278ba74c359533ffcfa9768be8
Patrick Ogenstad <patrick@ogenstad.com>
2018-04-06 18:40:07 +0200

    Fix setup.py issue when using pip 10. (#692)
    Fixes #689 pip10 compatibility issue

--- setup.py
+++ setup.py
@@ -1,13 +1,9 @@
 """setup.py file."""
-import uuid
-
 from setuptools import setup, find_packages
 
-from pip.req import parse_requirements
+with open("requirements.txt", "r") as fs:
+    reqs = [r for r in fs.read().splitlines() if (len(r) > 0 and not r.startswith("#"))]
 
-
-install_reqs = parse_requirements('requirements.txt', session=uuid.uuid1())
-reqs = [str(ir.req) for ir in install_reqs]
 
 __author__ = 'David Barroso <dbarrosop@dravetech.com>'
 
