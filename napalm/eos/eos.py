45aabafe22cf17a33f98ababa2a1731cb08cd93f
bewing <nicotine@warningg.com>
2018-09-17 15:15:14 -0500

    Update get_environment defaults if keys are missing when accessing power data (FIX#810)

--- napalm/eos/eos.py
+++ napalm/eos/eos.py
@@ -542,9 +542,9 @@ class EOSDriver(NetworkDriver):
         if not is_veos:
             for psu, data in power_output['powerSupplies'].items():
                 environment_counters['power'][psu] = {
-                    'status': data['state'] == 'ok',
-                    'capacity': data['capacity'],
-                    'output': data['outputPower']
+                    'status': data.get('state', 'ok') == 'ok',
+                    'capacity': data.get('capacity', -1.0),
+                    'output': data.get('outputPower', -1.0),
                 }
         cpu_lines = cpu_output.splitlines()
         # Matches either of
