41298f10aeddf59c19fd6a1ff5bbcfaff30279d4
Victor Pavlushin <41314137+VictorPavlushin@users.noreply.github.com>
2019-03-25 11:35:59 +0700

    Tox check black

--- napalm/nxos/nxos.py
+++ napalm/nxos/nxos.py
@@ -835,8 +835,10 @@ class NXOSDriver(NXOSDriverBase):
                 mac_address = None
             interfaces[interface_name] = {
                 "is_up": is_up,
-                "is_enabled": (interface_details.get("state") == "up" or
-                               interface_details.get("svi_admin_state", "") == "up"),
+                "is_enabled": (
+                    interface_details.get("state") == "up" or
+                    interface_details.get("svi_admin_state", "") == "up"
+                ),
                 "description": py23_compat.text_type(
                     interface_details.get("desc", "").strip('"')
                 ),
