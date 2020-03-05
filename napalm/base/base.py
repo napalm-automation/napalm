a9dc6779156581a88622e745938194ee893560cb
remingu <remingu@techturn.de>
2020-03-05 22:08:31 +0100

    removed enable() check from napalm.baseconnection, moved functionality to  driver. ios done,

--- napalm/base/base.py
+++ napalm/base/base.py
@@ -95,7 +95,6 @@ class NetworkDriver(object):
             raise ConnectionException("Cannot connect to {}".format(self.hostname))
 
         # ensure in enable mode
-        self._netmiko_device.enable()
         return self._netmiko_device
 
     def _netmiko_close(self):
