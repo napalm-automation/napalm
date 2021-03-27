024c85489ba2ee188201394cbda96759c25af3ee
Leo Kirchner <leo@kirchne.red>
2021-03-27 18:17:47 +0100

    Fix further pipeline errors

--- napalm/base/helpers.py
+++ napalm/base/helpers.py
@@ -303,7 +303,7 @@ def find_txt(
     return str(value)
 
 
-def convert(to: Callable[[T], R], who: Optional[T], default: R = "") -> R:
+def convert(to: Callable[[T], R], who: Optional[T], default: R = "") -> R:  # type: ignore
     """
     Converts data to a specific datatype.
     In case of error, will return a default value.
