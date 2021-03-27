ff87341db07863881763152089fcc9b9b64b087b
Leo Kirchner <leo@kirchne.red>
2021-03-27 18:24:53 +0100

    Convert all typing_extensions try/except imports to pure typing_extensions imports

--- napalm/base/base.py
+++ napalm/base/base.py
@@ -16,10 +16,7 @@ import sys
 from types import TracebackType
 from typing import Optional, Dict, Type, Any, List, Union
 
-try:
-    from typing import Literal
-except ImportError:
-    from typing_extensions import Literal  # type: ignore
+from typing_extensions import Literal
 
 from netmiko import ConnectHandler, NetMikoTimeoutException
 
