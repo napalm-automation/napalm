85a82096c1b22fdc6fa4dbed23b807823c7a731a
Shengan Zhang <zshengan@umich.edu>
2021-12-02 17:02:08 -0600

    reformat by black

--- test/base/validate/test_unit.py
+++ test/base/validate/test_unit.py
@@ -404,7 +404,9 @@ class TestValidate:
     @pytest.mark.parametrize("src, dst, result", _compare_getter)
     def test__compare_getter_list(self, src, dst, result):
         """Test for _compare_getter_list."""
-        assert validate.compare(copy.deepcopy(src), copy.deepcopy(dst)) == copy.deepcopy(result)
+        assert validate.compare(
+            copy.deepcopy(src), copy.deepcopy(dst)
+        ) == copy.deepcopy(result)
 
     def test_numeric_comparison(self):
         assert validate._compare_numeric("<2", 1)
