1a3a4c7f5cd0e784c0b8241f02bc55fc0a17d6a0
OsirisS13 <shaffwa@gmail.com>
2021-12-20 14:57:33 +1000

    Add support for optional_args to iosxr_netconf.py
    Added the ability to pass in optional arguments to the iosxr_netconf driver.  Note these are used as supplied and not passed through the netmiko_helpers base function.

--- napalm/iosxr_netconf/iosxr_netconf.py
+++ napalm/iosxr_netconf/iosxr_netconf.py
@@ -69,6 +69,7 @@ class IOSXRNETCONFDriver(NetworkDriver):
         if optional_args is None:
             optional_args = {}
 
+        self.netmiko_optional_args = optional_args
         self.port = optional_args.get("port", 830)
         self.lock_on_connect = optional_args.get("config_lock", False)
         self.key_file = optional_args.get("key_file", None)
@@ -91,6 +92,7 @@ class IOSXRNETCONFDriver(NetworkDriver):
                 key_filename=self.key_file,
                 timeout=self.timeout,
                 device_params={"name": "iosxr"},
+                **self.netmiko_optional_args,
             )
             if self.lock_on_connect:
                 self._lock()
@@ -442,7 +444,7 @@ class IOSXRNETCONFDriver(NetworkDriver):
             "is_up": False,
             "mac_address": "",
             "description": "",
-            "speed": -1.0,
+            "speed": -1,
             "last_flapped": -1.0,
         }
 
@@ -489,15 +491,14 @@ class IOSXRNETCONFDriver(NetworkDriver):
                 napalm.base.helpers.mac, raw_mac, raw_mac
             )
             speed = napalm.base.helpers.convert(
-                float,
+                int,
                 napalm.base.helpers.convert(
-                    float,
+                    int,
                     self._find_txt(interface_tree, "./int:bandwidth", namespaces=C.NS),
                     0,
                 )
                 * 1e-3,
             )
-
             mtu = int(
                 self._find_txt(interface_tree, "./int:mtu", default="", namespaces=C.NS)
             )
