1a7fe64157a6aa2ee35b4d300c688a0f80018609
Kirk Byers <ktbyers@twb-tech.com>
2022-10-17 13:08:23 -0700

    Trying to fix pytest7 breaking rootdir

--- docs/test.sh
+++ docs/test.sh
@@ -4,7 +4,7 @@ TEST_RESULTS_PATH="$CWD/support/tests"
 
 if [ ! -f "report.json" ]; then
     set -e
-    py.test -c /dev/null --cov=./ -vs --json=report.json ../test*/*/test_getters.py
+    py.test -c /dev/null --cov=./ -vs --json=report.json $CWD/../test*/*/test_getters.py
 
     set -e
     cp report.json $TEST_RESULTS_PATH/report.json
