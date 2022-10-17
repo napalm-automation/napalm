5b453685a45834b9b9ea65235fe6d143f1d20d94
Kirk Byers <ktbyers@twb-tech.com>
2022-10-17 13:43:02 -0700

    Fix py.test7 rootdir build issue with NAPALM docs (#1778)
    Co-authored-by: Ludovic Ortega <ludovic.ortega@adminafk.fr>

--- docs/test.sh
+++ docs/test.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 CWD=`pwd`
 TEST_RESULTS_PATH="$CWD/support/tests"
+REPOBASE=$CWD/..
 
 if [ ! -f "report.json" ]; then
     set -e
-    py.test -c /dev/null --cov=./ -vs --json=report.json ../test*/*/test_getters.py
+    py.test --rootdir $REPOBASE -c /dev/null --cov=./ -vs --json=report.json $REPOBASE/test*/*/test_getters.py
 
     set -e
     cp report.json $TEST_RESULTS_PATH/report.json
