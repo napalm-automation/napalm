3f3727370acb582f8002f6eb79f313e2586cc090
Kirk Byers <ktbyers@twb-tech.com>
2023-04-20 09:16:24 -0700

    Linting again

--- napalm/base/canonical_map.py
+++ napalm/base/canonical_map.py
@@ -1,4 +1,4 @@
 # Do not remove the below imports, functions were moved to netutils, but to not
 # break backwards compatibility, these should remain
-from netutils.constants import BASE_INTERFACES as base_interfaces   # noqa
-from netutils.constants import REVERSE_MAPPING as reverse_mapping   # noqa
+from netutils.constants import BASE_INTERFACES as base_interfaces  # noqa
+from netutils.constants import REVERSE_MAPPING as reverse_mapping  # noqa
