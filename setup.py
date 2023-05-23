26b7bfb9c4affe8f2f9408f933d28b24520a655a
Kirk Byers <ktbyers@twb-tech.com>
2023-05-23 10:23:36 -0700

    NAPALM 4.1.0 (#1935)

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ __author__ = "David Barroso <dbarrosop@dravetech.com>"
 
 setup(
     name="napalm",
-    version="4.0.0",
+    version="4.1.0",
     packages=find_packages(exclude=("test*",)),
     test_suite="test_base",
     author="David Barroso, Kirk Byers, Mircea Ulinic",
