napalm-automation/napalm

commit 77921f691a7dd099b1d44ba544d8d12db8f51b06

Explicitly close IOSXR connection (#2214)
* explicitly close IOSXR connection

* removed duplicate close

* Fixed linting issue

---------

Co-authored-by: Kirk Byers <ktbyers@twb-tech.com>

napalm/pyIOSXR/iosxr.py
@@ -510,8 +510,7 @@ class IOSXR(object):
         if self.lock_on_connect or self.locked:
             self.unlock()  # this refers to the config DB
         self._unlock_xml_agent()  # this refers to the XML agent
-        if hasattr(self.device, "remote_conn"):
-            self.device.remote_conn.close()  # close the underlying SSH session
+        self.device.disconnect()  # close the underlying SSH session
 
     def lock(self):
         """

test/pyiosxr/test_iosxr.py
@@ -35,6 +35,9 @@ class _MockedNetMikoDevice(object):
 
         self.remote_conn = _MockedParamikoTransport()
 
+    def disconnect(self):
+        self.remote_conn.close()
+
     @staticmethod
     def get_mock_file(command, format="xml"):
         filename = (

commit 2c18889b74399eb37c43fc449dbe310743add144

Clean up IOS-XR SSH sessions correctly.
This addresses the regression introduced by https://github.com/napalm-automation/napalm/pull/2214 and solves
https://github.com/napalm-automation/napalm/issues/2213

Without the two cleanup paths the Parmiko channel doesn't get closed correctly and it appears to make the processing hang. Combined
this cleans up the Paramiko channel and correctly closes and cleans up the SSH session on the IOSXR device.

Signed-off-by: Adrian Arumugam <adrian@a6m.au>

napalm/pyIOSXR/iosxr.py
@@ -510,7 +510,10 @@ class IOSXR(object):
         if self.lock_on_connect or self.locked:
             self.unlock()  # this refers to the config DB
         self._unlock_xml_agent()  # this refers to the XML agent
-        self.device.disconnect()  # close the underlying SSH session
+        # Both of the following methods are needed to cleanup the IOSXR connection successfully.
+        if hasattr(self.device, "remote_conn"):
+            self.device.remote_conn.close()  # close the underlying Paramiko/Netmiko channel.
+        self.device.disconnect()  # close the Netmiko session, inclusive of SSH cleanup if used.
 
     def lock(self):
         """